fc2e844a21b89f491026243b0ebb1c02d346f258
Teresa Soria <terence_line@hotmail.fr>
2022-10-17 19:32:35 +0200

    ajout header

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import logo from '../assets/logo.svg'
+import { Link } from "react-router-dom"
+import '../styles/Header.css'
+
+function Header ()
+{
+    return (
+        <div className="header">
+            <img src={ logo } alt="logo Kasa" className="header-logo" />
+            <nav className="header-links">
+                <Link to="/accueil">Accueil</Link>
+                <Link to="/apropos">A propos</Link>
+            </nav>
+        </div>
+    )
+
+}
+
+export default Header
+
+
