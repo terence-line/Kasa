ade57ac3af3dc5a0ed0853b68c01e63c69561b20
Teresa Soria <terence_line@hotmail.fr>
2022-10-18 00:47:54 +0200

    ajout banner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,22 @@
 //import { useState, useEffect } from "react"
 import React from "react"
-import Header from '../components/Header'
 import Card from '../components/Card'
 import '../styles/Home.css'
+import '../assets/eric-muhr-P_XxsdVgtpQ-unsplash.jpg'
 
 function Home ()
 {
     return (
         <div>
-            <Header />
             <div className="body-home">
+
+                <div className="banner">
+                    <h1>Chez vous, partout et ailleurs</h1>
+                    <div className="masked">
+                        <img src='../assets/eric-muhr-P_XxsdVgtpQ-unsplash.jpg'
+                            alt="paysage montagne au bord de l'eau" className="paysage1" />
+                    </div>
+                </div>
                 <Card />
             </div>
         </div>
