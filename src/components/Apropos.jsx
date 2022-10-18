aa1e6a02b74a4747b6366077117f2db4ba301d2a
Teresa Soria <terence_line@hotmail.fr>
2022-10-18 10:40:02 +0200

    modification apropos

--- src/components/Apropos.jsx
+++ src/components/Apropos.jsx
@@ -1,29 +1,29 @@
 import React from "react"
 import '../styles/Apropos.css'
+import image from '../assets/kalen-emsley-Bkci_8qcdvQ-unsplash 2.jpg'
 
 function Apropos ()
 {
 
     return (
-        <div className="container-apropos">
-            <div className="fiabilite">
-                <p>Fiabilité</p>
-                <div className="respect">
-                    <h1>Respect</h1>
-                </div>
-                <div className="service">
-                    <h1>Service</h1>
-                    <div className="responsabilite">
-                        <h1>Responsabilité</h1>
-                        <p>La bienveillance fait partie des valeurs fondatrices de Kasa.
-                            Tout comportement discriminatoire ou de perturbation du voisinage entraînera
-                            une exclusion de notre plateforme.
-                        </p>
-                        <span>Miguel</span>
-                    </div>
-                </div>
+        <div>
+            <div className="banner-apropos">
+                <img src={ image } alt="paysage montagne" className="paysage2" />
             </div>
 
+            <div className="fiabilite">
+                <h1>Fiabilité</h1>
+            </div>
+            <div className="respect">
+                <h1>Respect</h1>
+            </div>
+            <div className="service">
+                <h1>Service</h1>
+            </div>
+            <div className="responsabilite">
+                <h1>Responsabilité</h1>
+            </div>
+            <span>Miguel</span>
         </div>
     )
 }
