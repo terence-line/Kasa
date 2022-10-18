07aae1bb1fa8bb0780e6fa42627ddc3124aa9ffe
Teresa Soria <terence_line@hotmail.fr>
2022-10-18 13:50:52 +0200

    modification error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import '../styles/App.css';
 import Header from './Header'
 import Home from './Home'
 import Apropos from './Apropos'
 import FicheLogement from './FicheLogement'
 import Footer from './Footer'
+import Error from './Error'
 
+
+
+/*Switch  nous permet d'afficher uniquement la première route dont le chemin correspond, 
+et on ajoute une route à laquelle on ne passe pas de prop  path.
+En indiquant une route sans path à la fin du Switch, cela permet de capturer toutes les routes 
+dont le path ne correspond à aucune route déclarée, et donc de créer une page 404.*/
 
 function App ()
 {
@@ -14,9 +21,12 @@ function App ()
     <div className="container">
       <Router>
         <Header />
-        <Route path="/" exact component={ Home } />
-        <Route path="/apropos" exact component={ Apropos } />
-        <Route path="/logement/:id" component={ FicheLogement } />
+        <Switch>
+          <Route path="/" exact component={ Home } />
+          <Route path="/apropos" exact component={ Apropos } />
+          <Route path="/logement/:id" component={ FicheLogement } />
+          <Route path="*" component={ Error } />
+        </Switch>
         <Footer />
       </Router>
 
--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,22 +1,21 @@
 import React from "react"
-import logo from '../assets/logo.png'
+import logo from '../assets/404.svg'
 import { Link } from "react-router-dom"
 import '../styles/Error.css'
-import Header from '../components/Header'
+
 
 function Error ()
 {
     return (
+
         <div>
-            <Header />
-            <div className='error'>
-                <img src={ logo } alt="erreur 404" classname="error-logo" />
-                <h1>Oups! La page que vous demandez n'existe pas.</h1>
-                <Link className="p" to="/">
-                    Retourner sur la page d’accueil
-                </Link>
-            </div>
+            <img src={ logo } alt="erreur 404" classname="error-logo" />
+            <h1>Oups! La page que vous demandez n'existe pas.</h1>
+            <Link className="p" to="/">
+                Retourner sur la page d’accueil
+            </Link>
         </div>
+
     )
 }
 
