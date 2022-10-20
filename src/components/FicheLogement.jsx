6770376930f7ec25b9a7c85898e0a55323834e79
Teresa Soria <terence_line@hotmail.fr>
2022-10-20 16:53:13 +0200

    modification fiche logement et ajout icons slide

--- src/components/FicheLogement.jsx
+++ src/components/FicheLogement.jsx
@@ -12,7 +12,7 @@ import iconStar2 from '../assets/star-red.svg'
 
 function FicheLogement ()
 {
-    const [ open, setOpen ] = useState( false )
+    const [ open, setOpen ] = useState( true )
 
 
     return (
@@ -47,12 +47,15 @@ function FicheLogement ()
                 <div className='description'>
                     <h3>Description</h3>
                     <button className="btn" onClick={ icon }><img src={ icon } alt="chevron" className="chevron2" /></button>
+
                     { open && (
-                        <div className="text1">
-                            Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux
-                            bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus.
-                            Logement parfait pour les voyageurs en solo et les voyageurs d'affaires.
-                            Vous êtes à1 station de la gare de l'est (7 minutes à pied).
+                        <div className="container-text1">
+                            <p className="text1">
+                                Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux
+                                bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus.
+                                Logement parfait pour les voyageurs en solo et les voyageurs d'affaires.
+                                Vous êtes à1 station de la gare de l'est (7 minutes à pied).
+                            </p>
                         </div>
                     ) }
                 </div>
@@ -60,21 +63,23 @@ function FicheLogement ()
                     <h3>Équipements</h3>
                     <button className="btn" onClick={ icon }><img src={ icon } alt="chevron" className="chevron3" /></button>
                     { open && (
-                        <div className="text2">
-                            Climatisation
-                            Wi-Fi
-                            Cuisine
-                            Espace de travail
-                            Fer à repasser
-                            Sèche-cheveux
-                            Cintres
+                        <div className="container-text2">
+                            <div className="text2">
+                                Climatisation<br />
+                                Wi-Fi<br />
+                                Cuisine<br />
+                                Espace de travail<br />
+                                Fer à repasser<br />
+                                Sèche-cheveux<br />
+                                Cintres
+                            </div>
                         </div>
                     ) }
                 </div>
 
-            </div>
+            </div >
 
-        </div>
+        </div >
     )
 }
 
