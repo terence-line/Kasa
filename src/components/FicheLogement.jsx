3a8a49d1c142ab1e75f2287127cdffda0b41b20a
Teresa Soria <terence_line@hotmail.fr>
2022-10-20 15:24:01 +0200

    modification fiche logements

--- src/components/FicheLogement.jsx
+++ src/components/FicheLogement.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import '../styles/FicheLogement.css'
 import Carroussel from './Carroussel'
 import icon from '../assets/chevron.png'
@@ -14,6 +13,8 @@ import iconStar2 from '../assets/star-red.svg'
 function FicheLogement ()
 {
     const [ open, setOpen ] = useState( false )
+
+
     return (
         <div>
 
@@ -46,10 +47,29 @@ function FicheLogement ()
                 <div className='description'>
                     <h3>Description</h3>
                     <button className="btn" onClick={ icon }><img src={ icon } alt="chevron" className="chevron2" /></button>
+                    { open && (
+                        <div className="text1">
+                            Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux
+                            bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus.
+                            Logement parfait pour les voyageurs en solo et les voyageurs d'affaires.
+                            Vous êtes à1 station de la gare de l'est (7 minutes à pied).
+                        </div>
+                    ) }
                 </div>
                 <div className="equipements">
                     <h3>Équipements</h3>
                     <button className="btn" onClick={ icon }><img src={ icon } alt="chevron" className="chevron3" /></button>
+                    { open && (
+                        <div className="text2">
+                            Climatisation
+                            Wi-Fi
+                            Cuisine
+                            Espace de travail
+                            Fer à repasser
+                            Sèche-cheveux
+                            Cintres
+                        </div>
+                    ) }
                 </div>
 
             </div>
