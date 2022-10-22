275d6fa98240820537f8fba3e73f71c5a0e015ad
Teresa Soria <terence_line@hotmail.fr>
2022-10-22 23:26:32 +0200

    modification collapse

--- src/components/Apropos.jsx
+++ src/components/Apropos.jsx
@@ -15,9 +15,9 @@ function Apropos ()
             </div>
 
             <div className="container_collapse">
-                <Collapsible title="Fiabilité" content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes." /><br /><br />
-                <Collapsible title="Respect" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." /><br /><br />
-                <Collapsible title="Service" content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." /><br /><br />
+                <Collapsible title="Fiabilité" content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes." />
+                <Collapsible title="Respect" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+                <Collapsible title="Service" content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." />
                 <Collapsible title="Sécurité" content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
             </div>
 
--- src/components/Collapsible.jsx
+++ src/components/Collapsible.jsx
@@ -5,18 +5,18 @@ import icon from '../assets/chevron.png'
 function Collapsible ( { title, content } )
 {
     const [ isOpen, setIsOpen ] = useState( false )
-    const handleToggle = () =>
-    {
-        setIsOpen( !isOpen )
-    }
+    
 
     return (
 
         <div className={ "collapse" + ( isOpen ? 'open' : '' ) }>
-            <div className="collapse_title" onClick={ handleToggle }>
-                { title } <span className="collapse_icon"><img src={ icon } alt="chevron" /></span>
+            <div className="collapse_title" onClick={ () => setIsOpen(!isOpen)}>
+                { title } <img src={ icon } alt="chevron" className="collapse_icon" />
             </div>
-            <div className="collapse_content">{ content }</div>
+
+            { isOpen &&
+                <div className="collapse_content">{ content }</div>
+            }
         </div>
     )
 }
