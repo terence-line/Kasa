b4c5560c9e6014c940977015129302ffb249c8a2
ishigo1987 <stephnguets@yahoo.fr>
2022-10-25 14:58:06 +0100

    Mise a jour du fichier fichelogement.jsx

--- src/pages/FicheLogement.jsx
+++ src/pages/FicheLogement.jsx
@@ -9,58 +9,69 @@ import logements from '../datas/logements.json'
 
 function FicheLogement ()
 {
-    { logements.map( ( logements ) => (
-        <li key={ logements.id } > { logements.title }</li>
-    ) ) }
+  
+     const logementId = document.location.pathname.split("/").pop();
 
-    return (
+     const logementInfos = logements.find((logement)=>{
+
+            return logement.id === logementId;
+
+     });
+
+     
+
+    // { logements.map( ( logements ) => (
+    //     <li key={ logements.id } > { logements.title }</li>
+    // ) ) }
+
+    // return (
         
-        <div className="container-fichelogement">
+    //     <div className="container-fichelogement">
             
-            <Carroussel />
+    //         <Carroussel />
            
             
-            <div className="title">
-                <h1>{ logements.title }</h1>
-                <p>{ logements.location }</p>
-            </div>
+    //         <div className="title">
+    //             <h1>{ logements.title }</h1>
+    //             <p>{ logements.location }</p>
+    //         </div>
 
-            <div className="host">
-                <span className="span-host">{ logements.name}</span>
-                <div className='host-picture'>
-                    <img src={logements.picture} alt="photo de profil" />
-                </div>
-            </div>
+    //         <div className="host">
+    //             <span className="span-host">{ logements.name}</span>
+    //             <div className='host-picture'>
+    //                 <img src={logements.picture} alt="photo de profil" />
+    //             </div>
+    //         </div>
 
-            <div className='tags'>
-                <ul>
-                    <li>{logements.tags }</li>
-                    <li>Canal</li>
-                    <li>Paris 10</li>
-                </ul>
-            </div>
+    //         <div className='tags'>
+    //             <ul>
+    //                 <li>{logements.tags }</li>
+    //                 <li>Canal</li>
+    //                 <li>Paris 10</li>
+    //             </ul>
+    //         </div>
             
-            <div className="rate">
-                {logements.rating}
-                    <img src={ iconStar2 } alt="star" className="star1" />
-                    <img src={ iconStar2 } alt="star" className="star2" />
-                    <img src={ iconStar2 } alt="star" className="star3" />
-                    <img src={ iconStar1 } alt="star" className="star4" />
-                    <img src={ iconStar1 } alt="star" className="star5" />
-            </div>
+    //         <div className="rate">
+    //             {logements.rating}
+    //                 <img src={ iconStar2 } alt="star" className="star1" />
+    //                 <img src={ iconStar2 } alt="star" className="star2" />
+    //                 <img src={ iconStar2 } alt="star" className="star3" />
+    //                 <img src={ iconStar1 } alt="star" className="star4" />
+    //                 <img src={ iconStar1 } alt="star" className="star5" />
+    //         </div>
             
-             <div className="collapse-fichelogement">
-                <div className='collapse-fichelogement-item'>
-                    <Collapsible />
-                </div>
-                <div className='collapse-fichelogement-item'>
-                    <Collapsible />
-                </div>
-            </div>
+    //          <div className="collapse-fichelogement">
+    //             <div className='collapse-fichelogement-item'>
+    //                 <Collapsible />
+    //             </div>
+    //             <div className='collapse-fichelogement-item'>
+    //                 <Collapsible />
+    //             </div>
+    //         </div>
 
-        </div>  
+    //     </div>  
         
-    )
+    // )
 }
 
 export default FicheLogement
