b2fa3ff746e4345f0458f79ccf307adfefaf66d7
Teresa Soria <terence_line@hotmail.fr>
2022-10-26 14:16:47 +0200

    modification text dropdowm fichelogement

--- src/pages/FicheLogement.jsx
+++ src/pages/FicheLogement.jsx
@@ -61,13 +61,7 @@ function FicheLogement ()
                             <Dropdown title="Description" text="Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7 minutes à pied)." />
                      </div>
                      <div className='dropdown-fichelogement-item'>
-                             <Dropdown title="Equipements" text="Climatisation
-                             Wi-Fi
-                             Cuisine
-                             Espace de travail
-                             Fer à repasser
-                             Sèche-cheveux
-                             Cintres" />
+                             <Dropdown title="Equipements" text="Climatisation Wi-Fi Cuisine Espace de travail Fer à repasser Sèche-cheveux  Cintres" />
                      </div>
                </div>
 
