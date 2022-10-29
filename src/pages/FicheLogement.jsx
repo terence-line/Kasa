8addcdf25a450a90a57821a47a7858a60ad3a9a1
Teresa Soria <terence_line@hotmail.fr>
2022-10-29 15:06:27 +0200

    lmodification fichelogement description

--- src/pages/FicheLogement.jsx
+++ src/pages/FicheLogement.jsx
@@ -36,7 +36,7 @@ function FicheLogement ()
             
                      <div className="dropdown-fichelogement">
                             <div className='dropdown-fichelogement-item1'>
-                                   <Dropdown title="Description" text="Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7 minutes à pied)." />
+                                   <Dropdown title="Description" text={logement.description} />
                             </div>
 
                             
