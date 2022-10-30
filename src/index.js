33dcf92014b0f8d5f6f3750cc3062bd12ba63b11
Teresa Soria <terence_line@hotmail.fr>
2022-10-30 15:53:39 +0100

    suppression app transféré sur index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './pages/App'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import './styles/Index.css'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Apropos from './pages/Apropos'
+import FicheLogement from './pages/FicheLogement'
+import Error from './components/Error'
+import Footer from './components/Footer'
+
+
+/*Switch  nous permet d'afficher uniquement la première route dont le chemin correspond, 
+et on ajoute une route à laquelle on ne passe pas de prop  path.
+En indiquant une route sans path à la fin du Switch, cela permet de capturer toutes les routes 
+dont le path ne correspond à aucune route déclarée, et donc de créer une page 404.*/
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="container">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/apropos"  component={Apropos} />
+          <Route path="/logements/:id" component={FicheLogement} />
+          <Route path="*" component={Error} />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
   </React.StrictMode>
 )
 
